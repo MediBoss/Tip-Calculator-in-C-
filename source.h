7ae82c81c1cffd5a2bdbab70d9da26758520328c
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-29 23:34:04 -0500

    Add files via upload
    the source file, compelled and error-free

---
+++ source.h
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class tipCalc{
+
+    private:
+
+      double billAmount;// this variable will store the bill given by the waiter
+      int tipPercent; //this is will store how many percent of your money you want to tip the waiter
+
+    public:
+
+            //FUNCTIONS TO PROCESS THE CUSTOMER DATA
+
+      int getTipPercent() const; //simply retuns the percent of the tip mentioned by the customer
+      double getTipAmount() const;//returns the money to be given to the waiter as a tip
+      double getTotalAmount() const; //returns the total money to be paid including the tip
+      double getBillAmount() const;//simply returns the bill given by the waiter
+      std::string printBill() const; // prints the entire bill to the customer
+
+          //CONSTRUCTOR
+
+      tipCalc(double, int);
+
+};
